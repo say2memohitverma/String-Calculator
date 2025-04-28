ad058b983ef1d66e676173b85362563b662b5628
Mohit Verma <mverma@g2.com>
2025-04-28 23:19:50 +0530

    feat: Implement code to handle newline delimiter

--- src/calculator.ts
+++ src/calculator.ts
@@ -3,10 +3,7 @@ export function add(numbers: string): number {
       return 0;
     }
     
-    if (numbers.includes(",")) {
-      const nums = numbers.split(",").map(n => parseInt(n, 10));
-      return nums.reduce((sum, num) => sum + num, 0);
-    }
-    
-    return parseInt(numbers, 10);
+    const normalizedInput = numbers.replace(/\n/g, ",");
+    const nums = normalizedInput.split(",").map(n => parseInt(n, 10));
+    return nums.reduce((sum, num) => sum + num, 0);
   }
